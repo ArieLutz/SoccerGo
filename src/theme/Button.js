3237cbcdde15cdd7cad83e3253ea53f31e041d86
Saudyz <saudyzavala123@gmail.com>
2021-03-05 21:14:23 -0600

    componentes de la pantalla de regsitro

---
+++ src/theme/Button.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { StyleSheet, TouchableOpacity, Text, Dimensions } from "react-native";
+
+const { width } = Dimensions.get("screen");
+
+const Button = ({ title }) => {
+  return (
+      <Text style={styles.buttonText}>{title}</Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonText: {
+    alignSelf: "center",
+    backgroundColor: "#656873",
+    padding: 8,
+    borderRadius: 50,
+    marginBottom: 2,
+    marginTop: 15,
+    width: width * 0.7,
+    color: "#ffffff",
+    fontSize: 26,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
+
+export default Button;
---
+++ src/theme/index.js
@@ -0,0 +1,12 @@
+const theme ={
+   
+        colors: {
+          primary: "#9a98a6",
+          secondary: "",
+          //backgroundDark: "#ff0000",
+          //purple: " #ff0000",
+          green: "#373a40",
+        },
+};
+export default theme;
+
