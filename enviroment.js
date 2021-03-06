5a5b62f6afb6d7536ebfe031dbc64fdfec8a9354
Ariel Lutz <ariellutz10@gmail.com>
2021-03-05 23:59:59 -0600

    Archivos de Proyecto

--- enviroment.js
+++ enviroment.js
@@ -1,29 +1,27 @@
-import Constant from "Expo-constants";
+import Constant from "expo-constants";
 
 const ENV = {
     dev: {
-        apiKey: "AIzaSyCag-hEEwe_zA2WQbs2MkNG2g68oKZhN_Y",
-        authDomain: "soccer-go-app.firebaseapp.com",
-        databaseURL: "https://soccer-go-app-default-rtdb.firebaseio.com",
-        projectId: "soccer-go-app",
-        storageBucket: "soccer-go-app.appspot.com",
-        messagingSenderId: "511030700413",
-        appId: "1:511030700413:web:bfe1a9a7e6a43bbd50c414",
+        webAPIkey : "16026afe5861dc609f4bd454f72241d2", //KEY de la API 
+        apiURL :    "https://es.besoccer.com/api"
     },
     production: {
-        apiKey: "AIzaSyCag-hEEwe_zA2WQbs2MkNG2g68oKZhN_Y",
-        authDomain: "soccer-go-app.firebaseapp.com",
-        databaseURL: "https://soccer-go-app-default-rtdb.firebaseio.com",
-        projectId: "soccer-go-app",
-        storageBucket: "soccer-go-app.appspot.com",
-        messagingSenderId: "511030700413",
-        appId: "1:511030700413:web:bfe1a9a7e6a43bbd50c414",
+        webAPIkey : "16026afe5861dc609f4bd454f72241d2", //KEY de la API 
+        apiURL :    "https://es.besoccer.com/api"
+
     }
 };
 
-const getEnvVars = ( env = Constant.manifest.releaseChannel) =>{
-    if (__DEV__) return env.dev;
-    else if (env == "production" || env == "default") returnENV.production;
+const getEnvVars = (env = Constant.manifest.releaseChannel) => {
+    //  esta variable va establecer verdadero cuando react-native este corriendo
+    // en modo desarrollador y falso cuando sea publicado
+    if (__DEV__)  return ENV.dev;
+       
+    else if (env === "production" || env === "default")  return ENV.production;
+       
+       
+
 };
+
 
 export default getEnvVars;
