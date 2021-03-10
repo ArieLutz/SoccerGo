1422f61eee894091206e110d9c1716a823da8961
Ariel Lutz <ariellutz10@gmail.com>
2021-03-09 22:08:42 -0600

    Archivos de Programa

--- App.js
+++ App.js
@@ -3,19 +3,14 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import login from "./src/screens/login";
-import pantalla_prueba from "./src/screens/pantalla_prueba"
+import TabBarNavigation from "./src/screens/TabBarNavigation";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
       <SafeAreaProvider>
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name="login" component={login} options={{ headerShown: false }}/>
-            <Stack.Screen name="pantalla_prueba" component={pantalla_prueba} options={{ headerShown: false }}/>
-          </Stack.Navigator>
-        </NavigationContainer>
+        <TabBarNavigation/>
       </SafeAreaProvider>
   );
 }
