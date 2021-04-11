a0c4a9f677dacb00bfa18fb571ad8774acaf24f1
Chriss-1 <mejiachristian91@gmail.com>
2021-04-10 19:26:16 -0500

    Creando el diseño de la pantalla de crear comentario

--- src/screens/CreateComment.js
+++ src/screens/CreateComment.js
@@ -1,29 +1,43 @@
 import React, { useContext, useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import { Caption, IconButton, TextInput, Colors } from "react-native-paper";
 import { format } from "date-fns";
-import { Context as NoteContext } from "../providers/CommentContext";
+import { Context as CommentContext } from "../providers/CommentContext";
 import { Context as AuthContext } from "../providers/AuthContext";
 
 
-const CreateNote = ({ navigation }) => {
-  const { createNote } = useContext(NoteContext);
+const CreateComment = ({ navigation }) => {
+  const { createComment } = useContext(CommentContext);
   const { state } = useContext(AuthContext);
-  const [title, setTitle] = useState("");
   const [timestamp, setTimestamp] = useState(Date.now());
   const [content, setContent] = useState("");
 
   const handleSaveNote = () => {
     if (!title) {
       setTitle("New note");
-      createNote("New note", content, timestamp, state.user.id);
-    } else createNote(title, content, timestamp, state.user.id);
+      createComment("New note", timestamp, content, state.user.id);
+    } else createComment('prueba1', 'prueba2', timestamp, content, state.user.id);
 
     navigation.navigate("Home");
   };
 
   return (
     <View style={styles.container}>
+      <View style={styles.team}>
+        <Text style={styles.labelTeam}>Equipo1</Text>
+        <Text style={styles.labelTeam}> vs </Text>
+        <Text style={styles.labelTeam}>Equipo2</Text>
+      </View>
+      <Caption>{`${format(timestamp, "eee H:m")}, | ${
+        content.length
+      } characters`}</Caption>
+      <TextInput
+        multiline
+        style={styles.contentInput}
+        textAlignVertical="top"
+        value={content}
+        onChangeText={setContent}
+      />
       <View style={styles.iconBar}>
         <IconButton
           icon="close-circle-outline"
@@ -38,23 +52,6 @@ const CreateNote = ({ navigation }) => {
           onPress={handleSaveNote}
         />
       </View>
-      <TextInput
-        mode="flat"
-        placeholder="Title"
-        style={styles.titleInput}
-        value={title}
-        onChangeText={setTitle}
-      />
-      <Caption>{`${format(timestamp, "eee H:m")}, | ${
-        content.length
-      } characters`}</Caption>
-      <TextInput
-        multiline
-        style={styles.contentInput}
-        textAlignVertical="top"
-        value={content}
-        onChangeText={setContent}
-      />
     </View>
   );
 };
@@ -62,18 +59,27 @@ const CreateNote = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10,
-    backgroundColor: "#fff",
+    backgroundColor: "#656873",
   },
-  titleInput: {
+  team: {
+    flexDirection: "row",
+    backgroundColor: "#182126",
+    justifyContent: 'center',
+    width:'100%',
+    height:'10%',
+    
+  },
+  labelTeam:{
     fontSize: 22,
     fontWeight: "bold",
-    backgroundColor: "#656873",
+    color:'#fff',
+    paddingTop: '10px',
   },
   contentInput: {
     flex: 1,
     borderBottomWidth: 0,
     backgroundColor: "#656873",
+    height:'100%',
   },
   iconBar: {
     paddingTop: 10,
@@ -82,4 +88,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default CreateNote;
+export default CreateComment;
