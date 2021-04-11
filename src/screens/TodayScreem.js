d4cf981bb3be93f30f09c8d15a69af2b4aa1923d
Ariel Lutz <ariellutz10@gmail.com>
2021-04-10 21:53:51 -0600

    Prueba para consumir datos de la API

--- src/screens/TodayScreem.js
+++ src/screens/TodayScreem.js
@@ -1,5 +1,5 @@
-import React from "react";
-import React,{  useState, useEffect} from "react";
+
+import React ,{  useState, useEffect} from "react";
 import { View, Text, StyleSheet } from 'react-native';
 import { firebase } from "../../src/firebase";
 import { Button } from 'react-native-elements';
@@ -18,7 +18,47 @@ const TodayScreem = ({ navigation }) => {
         });
     }
 
+      //maneja el estado de la informacion de covid en las peticiones
+      const [EquiposPorId, setEquiposPorId] = useState(null);
 
+      const [errorConsulta, seterrorConsulta] = useState(false); //variable para el estado del try catch
+
+      
+      //CONSULTA A LA API
+              // Las peticiones se hacen mediante funciones asincronas(cualquier momento)
+              const getEquiposPorId = async () => {
+                try {
+                    //Consultar a la API de Covid19
+                    //nuestros valores para este backend Traer la información de el mundo
+                    const response = await index.get(`?&met=Teams&teamId=2616&APIkey=6925361e79865bba9729032b0eab2563f96f361a41514c4b315672126f99ea5b`); 
+                    // aqui la variable de estado ya recibio los valores de la peticion
+                    setEquiposPorId(response.data);        
+                } catch (errorConsulta) {
+                    //errorConsulta al momento de ejecutar la peticion
+                    seterrorConsulta(true);
+                }
+            };
+    
+      // Efecto secundario que ejecuta la consulta a la API
+
+      useEffect(() => {
+        getEquiposPorId();
+
+    }, []);
+
+    //los componentes se renderizan antes de ser mostrados y nunca
+        //deben retornar null
+        if (!EquiposPorId) {
+            return (
+              <Content>
+                <Spinner color="blue"/>
+              </Content>
+            )
+          }
+
+    
+    console.log(getEquiposPorId);
+    
     return (
         <View style={styles.container}>
             <Text style={styles.TextToday}>
