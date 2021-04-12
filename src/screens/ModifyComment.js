44212fd8848a690b931b881af9164328a26d45d9
Saudyz <saudyzavala123@gmail.com>
2021-04-12 11:58:03 -0600

    Pantalla de modificar funcional

--- src/screens/ModifyComment.js
+++ src/screens/ModifyComment.js
@@ -1,66 +1,101 @@
 import React, { useContext, useEffect, useState } from "react";
-import { StyleSheet, View } from "react-native";
-import { Caption, IconButton, TextInput } from "react-native-paper";
+import { StyleSheet, View, Text } from "react-native";
+import { Caption, IconButton, TextInput, Colors } from "react-native-paper";
 import { format } from "date-fns";
-import { Context as NoteContext } from "../providers/CommentContext";
+import { Context as CommentContext } from "../providers/CommentContext";
 import { Context as AuthContext } from "../providers/AuthContext";
+import Alert from "../shared/Alert";
 
-const ModifyNote = ({ navigation }) => {
-  const { state: notesState, updateNote } = useContext(NoteContext);
-  const { state } = useContext(AuthContext);
-  const [title, setTitle] = useState("");
+
+
+const ModifyComment = ({ navigation }) => {
+  const { state: commentsState, updateComment } = useContext(CommentContext);
+  const { state, clearErrorMessage } = useContext(AuthContext);
   const [timestamp, setTimestamp] = useState(Date.now());
   const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+  const [contentError, setContentError] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    if (notesState.currentNote.id) {
-      setTitle(notesState.currentNote.title);
-      setContent(notesState.currentNote.content);
+    if (commentsState.currentcomment.id) {
+      setTitle(commentsState.currentcomment.Equipo1);
+      setContent(commentsState.currentcomment.contenido);
     }
-  }, [notesState.currentNote]);
 
-  const handleSaveNote = () => {
-    updateNote(
-      notesState.currentNote.id,
-      title ? title : "New note",
+  }, [commentsState.currentcomment]);
+
+  useEffect(() => {
+    if (state.errorMessage) clearErrorMessage();
+  }, []);
+
+  useEffect(() => {
+    if (state.errorMessage) setError(state.errorMessage);
+  }, [state.errorMessage]);
+
+  
+
+  // Verifica que se ingrese un contenido al comentario
+  const handleVerify = (input) => {
+    if (input === "content") {
+      if (!content) setContentError(true);
+      else setContentError(false);
+    }
+    navigation.navigate("TodayScreem");
+  };
+
+
+  const handleSaveComment = () => {
+    updateComment(
+      commentsState.currentcomment.id,
       content,
       timestamp
     );
   };
 
   return (
+    
     <View style={styles.container}>
-      <View style={styles.iconBar}>
-        <IconButton
-          icon="close-circle-outline"
-          color={theme.colors.primary}
-          onPress={() => {
-            navigation.goBack();
-          }}
-        />
-        <IconButton
-          icon="check-circle-outline"
-          color={theme.colors.primary}
-          onPress={handleSaveNote}
-        />
+      
+      <View style={styles.team}>
+        <Text style={styles.labelTeam}>Equipo1</Text>
+        <Text style={styles.labelTeam}> vs </Text>
+        <Text style={styles.labelTeam}>Equipo2</Text>
       </View>
-      <TextInput
-        mode="flat"
-        placeholder="Title"
-        style={styles.titleInput}
-        value={title}
-        onChangeText={setTitle}
-      />
+
       <Caption>{`${format(timestamp, "eee H:m")}, | ${
         content.length
       } characters`}</Caption>
+      {error ? <Alert title={error} type="error" /> : null}
       <TextInput
         multiline
         style={styles.contentInput}
         textAlignVertical="top"
         value={content}
         onChangeText={setContent}
+        onBlur={() => {
+          handleVerify('content');
+        }}
+        errorMessage={
+          contentError
+            ? "Por favor ingrese un comentario"
+            : null
+        }
       />
+      <View style={styles.iconBar}>
+        <IconButton
+          icon="close-circle-outline"
+          color={Colors.red500}
+          onPress={() => {
+            navigation.goBack();
+          }}
+        />
+        <IconButton
+          icon="check-circle-outline"
+          color={Colors.green500}
+          onPress={handleSaveComment}
+        />
+      </View>
     </View>
   );
 };
@@ -68,15 +103,27 @@ const ModifyNote = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10,
+    backgroundColor: "#656873",
   },
-  titleInput: {
+  team: {
+    flexDirection: "row",
+    backgroundColor: "#182126",
+    justifyContent: 'center',
+    width:'100%',
+    height:'10%',
+    
+  },
+  labelTeam:{
     fontSize: 22,
     fontWeight: "bold",
+    color:'#fff',
+    paddingTop: 10,
   },
   contentInput: {
     flex: 1,
     borderBottomWidth: 0,
+    backgroundColor: "#656873",
+    height:'90%',
   },
   iconBar: {
     paddingTop: 10,
@@ -85,4 +132,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default ModifyNote;
+export default ModifyComment;
