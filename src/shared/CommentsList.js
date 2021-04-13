7e2c9dcf2e0bb9aa4835e6f2c3cf985436402892
Chriss-1 <mejiachristian91@gmail.com>
2021-04-12 19:40:25 -0500

    Modulo necesario para mostrar todos los comentario

---
+++ src/shared/CommentsList.js
@@ -0,0 +1,53 @@
+import React, { useContext } from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Context as CommentContext } from "../providers/CommentContext";
+import Comment from "./Comment";
+
+const CommentsList = ({ comments }) => {
+
+  const emptyFlatList = (
+    <View style={styles.emptycomments}>
+      <Text>You don't have any note yet...</Text>
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={comments}
+        emptyFlatList={emptyFlatList}
+        numColumns={1}
+        renderItem={({ item }) => (
+          <>
+              <Comment
+                key={item.id}
+                Equipo1={item.Equipo1}
+                Equipo2={item.Equipo2}
+                content={item.contenido}
+                timestamp={item.timestamp}
+              />
+          </>
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  emptycomments: {
+    flex: 1,
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+});
+
+export default CommentsList;
